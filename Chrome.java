53b19bb39be91ca7f2c792f5d760b70387f4514f
sowmyaadd <107055075+sowmyaadd@users.noreply.github.com>
2025-07-19 19:09:10 +0530

    Add files via upload
    SowmyaAutomationTestingAssignment1

---
+++ Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String args[]) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortCutKey = 'C';
+		System.out.println(version);		
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseYear);
+		System.out.println(shortCutKey);
+	}
+
+}
