090c19b8dd3509e3bb116a4bd353c08159cce896
sowmyaadd <107055075+sowmyaadd@users.noreply.github.com>
2025-07-24 11:01:23 +0530

    Add files via upload

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class CheckNumberIsPositive {
+public static void main(String[] args) {
+	int i= 15;
+	if(i>0) {
+		System.out.println(i +"is a positive number");
+		}
+	else {
+		System.out.println(i +"is not a positive number");
+	}
+}
+}
